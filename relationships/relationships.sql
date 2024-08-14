b7ebe29355c642bc4c8ad99f2353b0d9e23f3d79
EC2 Default User <ec2-user@ip-172-31-21-7.eu-north-1.compute.internal>
2024-08-14 16:29:50 +0000

    PK and FK for book_details created

---
+++ relationships/relationships.sql
@@ -0,0 +1,10 @@
+/*Create a PK and a FK for the book_details table*/
+
+CREATE TABLE book_details (
+  id integer PRIMARY KEY,
+  book_isbn varchar(50) REFERENCES book(isbn) UNIQUE,
+  rating decimal,
+  language varchar(10),
+  keywords text[],
+  date_published date
+);
