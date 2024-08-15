4b89394fb5469b920d9562b9e11b006f65c4a69d
EC2 Default User <ec2-user@ip-172-31-21-7.eu-north-1.compute.internal>
2024-08-15 14:11:18 +0000

    Validate the constrains in PK and FK for the books_authors table

--- relationships/relationships.sql
+++ relationships/relationships.sql
@@ -84,4 +84,11 @@ CREATE TABLE books_authors (
   book_isbn varchar(50) REFERENCES book(isbn),
   author_email varchar(20) REFERENCES author(email),
   PRIMARY KEY (book_isbn, author_email)
-)
+);
+
+/*Validate the constrains in PK and FK for the books_authors table*/
+SELECT constraint_name, table_name, column_name 
+FROM information_schema.key_column_usage 
+WHERE table_name = 'page';
+
+
