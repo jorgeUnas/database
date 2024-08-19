781a69fe0c4ea31f40fcf2c177a9b9b62f7da406
EC2 Default User <ec2-user@ip-172-31-21-7.eu-north-1.compute.internal>
2024-08-19 10:46:06 +0000

    Creating the restaurant table

---
+++ relationships/restaurants.sql
@@ -0,0 +1,10 @@
+/*Creating the restaurant table*/
+
+CREATE TABLE restaurant (
+id integer,
+name varchar(20),
+description varchar(100),
+rating decimal,
+telephone char(10),
+hours varchar(100)
+);
