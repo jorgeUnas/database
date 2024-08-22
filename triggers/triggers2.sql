93befeeede052a77de57b0babaff17441f5f1625
EC2 Default User <ec2-user@ip-172-31-21-7.eu-north-1.compute.internal>
2024-08-22 09:37:06 +0000

    Create a trigger to update high spenders

---
+++ triggers/triggers2.sql
@@ -0,0 +1,11 @@
+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
+
+SELECT * FROM clients;
+
+/*Create a trigger to update high spenders*/
+
+CREATE TRIGGER update_trigger_high 
+BEFORE UPDATE ON clients
+FOR EACH ROW
+WHEN (NEW.total_spent >= 1000)
+EXECUTE PROCEDURE set_high_spender();
