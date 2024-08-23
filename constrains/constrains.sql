0ace4a82f539a2810df5f25e2fcf6e49ec83bc24
EC2 Default User <ec2-user@ip-172-31-21-7.eu-north-1.compute.internal>
2024-08-23 10:00:32 +0000

    Create an attendees table

--- constrains/constrains.sql
+++ constrains/constrains.sql
@@ -8,3 +8,12 @@ SELECT *
 FROM speakers
 LIMIT 10;
 
+/*Create an attendees table*/
+
+CREATE TABLE attendees  (
+    id integer,
+    name varchar,
+    total_tickets_reserved integer,
+    standard_tickets_reserved integer,
+    vip_tickets_reserved integer
+);
