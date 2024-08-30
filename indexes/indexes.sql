5a6bafe61a7d9f98b583d48772573b3b34019098
EC2 Default User <ec2-user@ip-172-31-21-7.eu-north-1.compute.internal>
2024-08-30 15:21:45 +0000

    Searching in columns with and without an index

--- indexes/indexes.sql
+++ indexes/indexes.sql
@@ -4,3 +4,7 @@ FROM customers;
 SELECT *
 FROM pg_Indexes
 WHERE tablename = 'customers';
+
+EXPLAIN ANALYZE SELECT * FROM customers WHERE first_name = 'David';
+
+EXPLAIN ANALYZE SELECT * FROM customers WHERE last_name = 'Jones';
